b0147ee48fd5cc160c4371b71a181dbd6b990f68
AleksandrSterne <lazdju@gmail.com>
2024-02-13 15:04:57 +0100

    account info test

--- src/app/components/account-info/account-info.component.spec.ts
+++ src/app/components/account-info/account-info.component.spec.ts
@@ -3,32 +3,6 @@ import { AccountInfoComponent } from './account-info.component';
 import { By } from '@angular/platform-browser';
 import { User } from '../../services/user.model';
 
-const _expectComponentInitialState = (
-  fixture: ComponentFixture<AccountInfoComponent>,
-  selector: string,
-  data: User
-) => {
-  fixture.detectChanges();
-
-  let header = fixture.debugElement.queryAll(
-    By.css(`${selector} .card-header h4`)
-  );
-  let body = fixture.debugElement.queryAll(
-    By.css(`${selector} .card-body div`)
-  );
-
-  expect(header.length).toBe(1);
-  expect(header[0].nativeElement.textContent).toContain('User info');
-  expect(body.length).toBe(3);
-  expect(body[0].nativeElement.textContent).toContain(`Id: ${data.id}`);
-  expect(body[1].nativeElement.textContent).toContain(
-    `Username: ${data.username}`
-  );
-  expect(body[2].nativeElement.textContent).toContain(
-    `Password: ${data.password}`
-  );
-};
-
 describe('AccountInfoComponent', () => {
   let component: AccountInfoComponent;
   let fixture: ComponentFixture<AccountInfoComponent>;
@@ -55,7 +29,23 @@ describe('AccountInfoComponent', () => {
 
   describe('Initial state', () => {
     it('should render component initial state', () => {
-      _expectComponentInitialState(fixture, '.__account-info', mockUser);
+      let header = fixture.debugElement.queryAll(
+        By.css('.__account-info .card-header h4')
+      );
+      let body = fixture.debugElement.queryAll(
+        By.css('.__account-info .card-body div')
+      );
+
+      expect(header.length).toBe(1);
+      expect(header[0].nativeElement.textContent).toContain('User info');
+      expect(body.length).toBe(3);
+      expect(body[0].nativeElement.textContent).toContain(`Id: ${mockUser.id}`);
+      expect(body[1].nativeElement.textContent).toContain(
+        `Username: ${mockUser.username}`
+      );
+      expect(body[2].nativeElement.textContent).toContain(
+        `Password: ${mockUser.password}`
+      );
     });
   });
 });
