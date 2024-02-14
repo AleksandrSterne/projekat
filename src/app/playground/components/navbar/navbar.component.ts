13a1cfab968ea8236f3b8b122ccfe67345336b63
AleksandrSterne <lazdju@gmail.com>
2024-02-14 04:43:54 +0100

    added kendo modules, navbar component

--- src/app/playground/components/navbar/navbar.component.ts
+++ src/app/playground/components/navbar/navbar.component.ts
@@ -1,13 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { IconsModule } from '@progress/kendo-angular-icons';
+import { AppBarModule } from '@progress/kendo-angular-navigation';
+import { SVGIcon, menuIcon } from '@progress/kendo-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [AppBarModule, IconsModule, RouterLink, ButtonsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {
-
+  public menuIcon: SVGIcon = menuIcon;
 }
