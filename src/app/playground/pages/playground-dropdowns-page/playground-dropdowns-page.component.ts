48065204e844898311542564ca5dd16f802e3215
AleksandrSterne <lazdju@gmail.com>
2024-02-20 03:12:00 +0100

    dropdowns

--- src/app/playground/pages/playground-dropdowns-page/playground-dropdowns-page.component.ts
+++ src/app/playground/pages/playground-dropdowns-page/playground-dropdowns-page.component.ts
@@ -1,11 +1,70 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { GroupResult, groupBy } from '@progress/kendo-data-query';
+import { storeProducts } from './store-products';
+import { searchIcon } from '@progress/kendo-svg-icons';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
 
 @Component({
   selector: 'app-playground-dropdowns-page',
   standalone: true,
-  imports: [],
+  imports: [DropDownsModule, ButtonsModule],
   templateUrl: './playground-dropdowns-page.component.html',
   styleUrl: './playground-dropdowns-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PlaygroundDropdownsPageComponent {}
+export class PlaygroundDropdownsPageComponent {
+  public areaList: Array<string> = [
+    'Amsterdam',
+    'Athens',
+    'Barcelona',
+    'Berlin',
+    'Brussels',
+    'Chicago',
+    'Copenhagen',
+    'Dublin',
+    'Helsinki',
+    'Houston',
+    'Lisbon',
+    'London',
+    'Los Angeles',
+    'Madrid',
+    'Miami',
+    'Montreal',
+    'New York',
+    'Paris',
+    'Philadelphia',
+    'Prague',
+    'Rome',
+    'Sao Paulo',
+    'Seattle',
+    'Stockholm',
+    'Toronto',
+    'Vancouver',
+    'Vienna',
+    'Vienna',
+    'Warsaw',
+  ];
+  public city = 'Los Angeles';
+  public data: Array<string>;
+  public groupedData: GroupResult[] = groupBy(storeProducts, [
+    { field: 'subcategory' },
+  ]) as GroupResult[];
+  public virtual: any = {
+    itemHeight: 16,
+  };
+  public itemDisabled(itemArgs: { dataItem: string; index: number }): boolean {
+    return itemArgs.index === 2;
+  }
+  public searchSVG = searchIcon;
+
+  constructor() {
+    this.data = this.areaList.slice();
+  }
+
+  handleFilter(value: string) {
+    this.data = this.areaList.filter(
+      (s) => s.toLowerCase().indexOf(value.toLowerCase()) !== -1
+    );
+  }
+}
